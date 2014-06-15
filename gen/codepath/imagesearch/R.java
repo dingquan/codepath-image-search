975c86bd69de1af2118a1cb7e26956dff71ac716
Quan Ding <quding@stubhub.com>
2014-06-14 17:09:38 -0700

    layout changes

--- gen/codepath/imagesearch/R.java
+++ gen/codepath/imagesearch/R.java
@@ -13,12 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int btnSearch=0x7f060001;
+        public static final int etQuery=0x7f060000;
+        public static final int gvImages=0x7f060002;
+    }
     public static final class layout {
         public static final int activity_search=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_search=0x7f040003;
         public static final int hello_world=0x7f040001;
+        public static final int search_hint=0x7f040002;
     }
     public static final class style {
         /** 
